dd6c92c5a41a41717ea4f446db2e1844a4b78c42
Diego Molina <diegomolina_30@hotmail.com>
2021-10-15 20:03:43 -0500

    Plataforma v1

--- src/js/Plataforma.js
+++ src/js/Plataforma.js
@@ -11,7 +11,7 @@ function Plataforma() {
       {/* <header>Este es el header</header> */}
       
       {/*SideBar*/}
-      <nav id="navbar" class="navbar navbar-light bg-light flex-column"> {/*align-items-stretch p-3*/}
+      <nav id="navbar" class="navbar"> {/*align-items-stretch p-3* class="navbar navbar-light bg-light flex-column"/}
         {/*<a class="navbar-brand" href="#">Navbar</a>*/}
         <div id="imagen">
 
@@ -24,9 +24,26 @@ function Plataforma() {
 
         <div id="opciones">
 
-          <button type="button" className="btn btn-success">Reglas</button>
-          <button type="button" className="btn btn-success">Diccionario</button>
-          <button type="button" className="btn btn-success">Diagrama</button>
+          <button type="button" className="btn btn-success" title="Iniciar diagnostico del paciente">
+            <i class="fas fa-diagnoses"></i>
+            Diagnosticar
+          </button>
+          
+          <button type="button" className="btn btn-success" title="Ver reglas de diagnostico">
+            <i class="fas fa-heartbeat"></i>
+            Sintomas
+            {/*Reglamento*/}
+          </button>
+
+          <button type="button" className="btn btn-success" title="Ver diccionario de terminos">
+            <i class="fas fa-book"></i>
+            Diccionario
+          </button>
+
+          <button type="button" className="btn btn-success" title="Ver diagrama de flujo">
+            <i class="fas fa-project-diagram"></i>
+            Diagrama
+          </button>
 
         </div>
 
@@ -40,25 +57,29 @@ function Plataforma() {
           <select class="form-select" title="Seleccione categoria para buscar palabra" >
             
             <option selected >Seleccione Categoria...</option> {/*disabled*/}
-            <option value="1">Regla</option>
+            <option value="1">Sintoma</option>
             <option value="2">Diccionario</option>
 
           </select>
           
-          <input type="text" placeholder="¿Busca algo puntual?" title="Ingrese la palabra a buscar"/>
+          {/*<input type="text" placeholder="¿Busca algo puntual?" title="Ingrese la palabra a buscar"/>*/}
+          <input id="usuario" type="text" className="form-control" placeholder="¿Busca algo puntual?" title="Ingrese la palabra a buscar"/>
 
         </div>
 
         {/*Contenedor botón inicio perfil*/}
         <div id="botonesSuperior" className="btn-group" role="group" >
 
-          <button type="button" className="btn btn-success" title="Regresar a la pagina de inicio">Inicio</button>
-          {/*<button type="button" className="btn btn-success">Middle</button>*/}
-
+          <button id="inicio" type="button" className="btn btn-success" title="Regresar a la pagina de inicio">
+            <i class="fas fa-home"></i>
+            Inicio
+          </button>
+          
           <button id="expandiblePerfil" type="button" class="btn btn-primary" data-bs-toggle="dropdown" aria-expanded="false" title="Opciones de perfil"> {/*class="btn btn-primary dropdown-toggle"*/}
             Perfil
             <i class="far fa-caret-square-down"></i>
           </button>
+
           <ul class="dropdown-menu" >
             <li><a class="dropdown-item" href="" >Ver perfil</a></li>
             <li><a class="dropdown-item" href="" >Salir</a></li>
@@ -70,7 +91,7 @@ function Plataforma() {
 
       <div id="contenido">
 
-        prueba
+        Contenido
 
       </div>
 
