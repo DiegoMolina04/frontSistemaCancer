7b459c519804f088f90e5cb0e1245b0501faf0ed
Diego Molina <diegomolina_30@hotmail.com>
2021-10-26 12:25:03 -0500

    Sintomas v1

--- src/js/Reglas.js
+++ src/js/Reglas.js
@@ -11,20 +11,20 @@ function Reglas() {
             <div id="cabeceraSintomasBoton-Reglas">
 
                 <div id="cajaLabelSintoma-Reglas">
-                    <label>Sintomas</label>
+                    <label>Síntomas</label>
                 </div>
                 
 
                 <div id="cajaAgregarSintoma-Reglas">
                     
                     <label id="labelAgregarSintoma-Reglas">
-                        ¿Desea agregar un nuevo sintoma?
+                        ¿Desea agregar un nuevo síntoma?
                     </label>
 
                     
                     <button id="agregarSintoma-Reglas" type="button" className="btn btn-success" title="Agregar un nuevo sintoma" data-bs-toggle="modal" data-bs-target="#modalAgregarSintoma-Reglas">
                         <i class="fas fa-plus-circle"></i>
-                         Agregar Sintoma
+                         Agregar Síntoma
                     </button>
 
 
@@ -41,10 +41,10 @@ function Reglas() {
                                     <h5 class="modal-title" id="modalLabelAgregarSintoma-Reglas">
 
                                         <i class="fas fa-plus-circle"></i>
-                                        <label >Agregar Nuevo Sintoma</label>
+                                        <label >Agregar Nuevo Síntoma</label>
 
                                     </h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" title="Regresa el diccionario de sintomas"></button>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" title="Regresa a la tabla de síntomas"></button>
                                 </div>
 
                                 <div class="modal-body">
@@ -59,7 +59,7 @@ function Reglas() {
                                     <div className="input-group">
                                         <div className="input-group-text" id="btnGroupAddon"><i class="fas fa-book"></i></div>
                                         
-                                        <textarea class="form-control" id="floatingTextarea" placeholder="Ingrese el sintoma" title="Nuevo sintoma"></textarea>
+                                        <textarea class="form-control" id="floatingTextarea" placeholder="Ingrese el síntoma" title="Nuevo síntoma"></textarea>
                                         
                                         
                                     </div>
@@ -75,9 +75,16 @@ function Reglas() {
 
                                     </div>
                                     
-                                    <div className="input-group">
-                                        <div className="input-group-text" id="btnGroupAddon"><i class="fas fa-font"></i></div>
-                                        <input id="" type="text" className="form-control" placeholder="Ingrese la clasificación" title="Nuevo clasificación" />
+                                    <div id="cajaRadioButtonModalAgregar-Reglas">
+
+                                    <div class="form-check form-check-inline"> {/**/}
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="radioButton" value="option1" title="Es una señal"/>
+                                        <p id="labelSeñalModalAgregar-Reglas" for="inlineRadio1">Señal</p>
+                                    </div>
+                                    <div class="form-check form-check-inline"> {/*form-check-inline*/}
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="radioButton" value="option2" title="Es un síntoma"/>
+                                        <p id="labelSintomaModalAgregar-Reglas" for="inlineRadio2">Síntoma</p>
+                                    </div>
                                     </div>
 
                                     <br />
@@ -85,8 +92,8 @@ function Reglas() {
                                 </div>
                                 <div class="modal-footer">
 
-                                  <button type="button" id="botonModalCancelar-Reglas" class="btn btn-secondary" data-bs-dismiss="modal" title="Regresa a la tabla de sintomas">Cancelar</button>
-                                  <button type="button" id="botonModalModificar-Reglas" class="btn btn-primary" title="Modificar sintoma">Modificar</button>
+                                  <button type="button" id="botonModalCancelar-Reglas" class="btn btn-secondary" data-bs-dismiss="modal" title="Regresa a la tabla de síntomas">Cancelar</button>
+                                  <button type="button" id="botonModalModificar-Reglas" class="btn btn-primary" title="Modificar síntoma">Modificar</button>
 
                                 </div>
                             </div>
@@ -106,7 +113,7 @@ function Reglas() {
                     <thead> {/*Cabeceras*/}
                         <tr>
                             <th id="idTablaCabecera-Reglas" scope="col" title="Identificador del sintoma">ID</th>
-                            <th id="sintomaTablaCabecera-Reglas" scope="col" title="Pregunta del sintoma">Sintoma</th>
+                            <th id="sintomaTablaCabecera-Reglas" scope="col" title="Pregunta del sintoma">Síntoma</th>
                             <th id="clasificacionTablaCabecera-Reglas" scope="col" title="Clasificación de la acción a tomar">Clasificación</th>
 
                             {/*Columna para agregar o elimina*/}
@@ -122,11 +129,11 @@ function Reglas() {
                             {/*Fila para los botones modificar o eliminar regla*/}
                             <td id="modificarEliminarTablaFila-Reglas">
 
-                                <button id="botonModificar-Reglas" type="button" class="btn btn-success" title="Modificar sintoma" data-bs-toggle="modal" data-bs-target="#modalModificar-Reglas">
+                                <button id="botonModificar-Reglas" type="button" class="btn btn-success" title="Modificar síntoma" data-bs-toggle="modal" data-bs-target="#modalModificar-Reglas">
                                     <i id="iconoModificar-Reglas" class="fas fa-cog"></i>
                                 </button>
                                 
-                                <button id="botonEliminar-Reglas" type="button" class="btn btn-success" title="Eliminar sintoma" data-bs-toggle="modal" data-bs-target="#modalEliminar-Reglas">
+                                <button id="botonEliminar-Reglas" type="button" class="btn btn-success" title="Eliminar síntoma" data-bs-toggle="modal" data-bs-target="#modalEliminar-Reglas">
                                     <i id="iconoEliminar-Reglas" class="fas fa-times-circle"></i>
                                 </button>
 
@@ -136,16 +143,16 @@ function Reglas() {
                         <tr>
                             <th id="idTablaFila-Reglas" scope="row">2</th>
                             <td id="sintomaTablaFila-Reglas">¿Un lunar existente ha cambiado de forma?</td>
-                            <td id="clasificacionTablaFila-Reglas">Sintoma</td>
+                            <td id="clasificacionTablaFila-Reglas">Síntoma</td>
 
                             {/*Fila para los botones modificar o eliminar regla*/}
                             <td id="modificarEliminarTablaFila-Reglas">
 
-                                <button id="botonModificar-Reglas" type="button" class="btn btn-success" title="Modificar sintoma" data-bs-toggle="modal" data-bs-target="#modalModificar-Reglas">
+                                <button id="botonModificar-Reglas" type="button" class="btn btn-success" title="Modificar síntoma" data-bs-toggle="modal" data-bs-target="#modalModificar-Reglas">
                                     <i id="iconoModificar-Reglas" class="fas fa-cog"></i>
                                 </button>
                                 
-                                <button id="botonEliminar-Reglas" type="button" class="btn btn-success" title="Eliminar sintoma" data-bs-toggle="modal" data-bs-target="#modalEliminar-Reglas">
+                                <button id="botonEliminar-Reglas" type="button" class="btn btn-success" title="Eliminar síntoma" data-bs-toggle="modal" data-bs-target="#modalEliminar-Reglas">
                                     <i id="iconoEliminar-Reglas" class="fas fa-times-circle"></i>
                                 </button>
                                 
@@ -156,16 +163,16 @@ function Reglas() {
                         <tr>
                             <th id="idTablaFila-Reglas" scope="row">3</th>
                             <td id="sintomaTablaFila-Reglas">¿La mitad de un lunar de nacimiento no es igual a la otra mitad?</td>
-                            <td id="clasificacionTablaFila-Reglas">Sintoma</td>
+                            <td id="clasificacionTablaFila-Reglas">Síntoma</td>
 
                             {/*Fila para los botones modificar o eliminar regla*/}
                             <td id="modificarEliminarTablaFila-Reglas">
 
-                                <button id="botonModificar-Reglas" type="button" class="btn btn-success" title="Modificar sintoma" data-bs-toggle="modal" data-bs-target="#modalModificar-Reglas">
+                                <button id="botonModificar-Reglas" type="button" class="btn btn-success" title="Modificar síntoma" data-bs-toggle="modal" data-bs-target="#modalModificar-Reglas">
                                     <i id="iconoModificar-Reglas" class="fas fa-cog"></i>
                                 </button>
                                 
-                                <button id="botonEliminar-Reglas" type="button" class="btn btn-success" title="Eliminar sintoma" data-bs-toggle="modal" data-bs-target="#modalEliminar-Reglas">
+                                <button id="botonEliminar-Reglas" type="button" class="btn btn-success" title="Eliminar síntoma" data-bs-toggle="modal" data-bs-target="#modalEliminar-Reglas">
                                     <i id="iconoEliminar-Reglas" class="fas fa-times-circle"></i>
                                 </button>
 
@@ -175,16 +182,16 @@ function Reglas() {
                         <tr>
                             <th id="idTablaFila-Reglas" scope="row">4</th>
                             <td id="sintomaTablaFila-Reglas">¿Los bordes del lunar son irregulares o dentados?</td>
-                            <td id="clasificacionTablaFila-Reglas">Sintoma</td>
+                            <td id="clasificacionTablaFila-Reglas">Síntoma</td>
 
                             {/*Fila para los botones modificar o eliminar regla*/}
                             <td id="modificarEliminarTablaFila-Reglas">
 
-                                <button id="botonModificar-Reglas" type="button" class="btn btn-success" title="Modificar sintoma" data-bs-toggle="modal" data-bs-target="#modalModificar-Reglas">
+                                <button id="botonModificar-Reglas" type="button" class="btn btn-success" title="Modificar síntoma" data-bs-toggle="modal" data-bs-target="#modalModificar-Reglas">
                                     <i id="iconoModificar-Reglas" class="fas fa-cog"></i>
                                 </button>
                                 
-                                <button id="botonEliminar-Reglas" type="button" class="btn btn-success" title="Eliminar sintoma" data-bs-toggle="modal" data-bs-target="#modalEliminar-Reglas">
+                                <button id="botonEliminar-Reglas" type="button" class="btn btn-success" title="Eliminar síntoma" data-bs-toggle="modal" data-bs-target="#modalEliminar-Reglas">
                                     <i id="iconoEliminar-Reglas" class="fas fa-times-circle"></i>
                                 </button>
 
@@ -207,14 +214,14 @@ function Reglas() {
                                 </div>
                                 <div class="modal-body">
                                   
-                                    <label>¿Realmente quiere eliminar este sintoma?</label>
+                                    <label>¿Realmente quiere eliminar este síntoma?</label>
                                     <label id="labelRevertirCambios-Reglas">Los cambios no se podrán revertir.</label>
 
                                 </div>
                                 <div class="modal-footer">
 
-                                  <button type="button" id="botonModalCancelar-Reglas" class="btn btn-secondary" data-bs-dismiss="modal" title="Regresa a la tabla de sintomas">Cancelar</button>
-                                  <button type="button" id="botonModalEliminar-Reglas" class="btn btn-primary" title="Eliminar sintoma">Eliminar</button>
+                                  <button type="button" id="botonModalCancelar-Reglas" class="btn btn-secondary" data-bs-dismiss="modal" title="Regresa a la tabla de síntomas">Cancelar</button>
+                                  <button type="button" id="botonModalEliminar-Reglas" class="btn btn-primary" title="Eliminar síntoma">Eliminar</button>
 
                                 </div>
                               </div>
@@ -231,20 +238,20 @@ function Reglas() {
                                   <h5 class="modal-title" id="modalLabelModificar-Reglas">
 
                                     <i class="fas fa-cog"></i>
-                                    <label >Modificar sintoma</label>
+                                    <label >Modificar síntoma</label>
 
                                   </h5>
-                                  <button type="button" class="btn-close" data-bs-dismiss="modal" title="Regresa a la tabla de sintomas"></button>
+                                  <button type="button" class="btn-close" data-bs-dismiss="modal" title="Regresa a la tabla de síntomas"></button>
                                 </div>
                                 <div class="modal-body">
 
                                    {/*Sección Sintoma*/}
-                                   <label>Sintoma</label>
+                                   <label>Síntoma</label>
                                     
                                     <div className="input-group">
                                         <div className="input-group-text" id="btnGroupAddon"><i class="fas fa-book"></i></div>
                                         
-                                        <textarea class="form-control" id="floatingTextarea" placeholder="Ingrese la nueva descripción" title="Descripción actual"></textarea>
+                                        <textarea class="form-control" id="floatingTextarea" placeholder="Ingrese el nuevo síntoma" title="Síntoma actual"></textarea>
                                         
                                         
                                     </div>
@@ -254,9 +261,18 @@ function Reglas() {
                                      {/*Sección Sintoma*/}
                                      <label>Clasificación</label>
                                     
-                                    <div className="input-group">
+                                    {/*<div className="input-group">
                                         <div className="input-group-text" id="btnGroupAddon"><i class="fas fa-font"></i></div>
-                                        <input id="" type="text" className="form-control" placeholder="Ingrese el nuevo sintoma" title="Sintoma actual" />
+                                        <input id="" type="text" className="form-control" placeholder="Ingrese el nuevo síntoma" title="Síntoma actual" />
+                                    </div>*/}
+
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="radioButton" value="option1" title="Es una señal"/>
+                                        <p id="labelSeñalModalModificar-Reglas" for="inlineRadio1">Señal</p>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="radioButton" value="option2" title="Es un sintoma"/>
+                                        <p id="labelSintomaModalModificar-Reglas" for="inlineRadio2">Síntoma</p>
                                     </div>
 
                                     <br />
@@ -264,8 +280,8 @@ function Reglas() {
                                 </div>
                                 <div class="modal-footer">
 
-                                  <button type="button" id="botonModalCancelar-Reglas" class="btn btn-secondary" data-bs-dismiss="modal" title="Regresa al Reglas de sintoma">Cancelar</button>
-                                  <button type="button" id="botonModalModificar-Reglas" class="btn btn-primary" title="Modificar sintoma">Modificar</button>
+                                  <button type="button" id="botonModalCancelar-Reglas" class="btn btn-secondary" data-bs-dismiss="modal" title="Regresa a la tabla de síntomas">Cancelar</button>
+                                  <button type="button" id="botonModalModificar-Reglas" class="btn btn-primary" title="Modificar síntoma">Modificar</button>
 
                                 </div>
                               </div>
