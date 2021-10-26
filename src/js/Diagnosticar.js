f325b467e4be0806f13d64a90aa8b3000947f71e
Diego Molina <diegomolina_30@hotmail.com>
2021-10-26 15:40:08 -0500

    Avance 2 en la vista Diagnosticar

--- src/js/Diagnosticar.js
+++ src/js/Diagnosticar.js
@@ -1,15 +1,35 @@
+import '../css/Diagnosticar.css';
+import '../css/fontawesome-free-5.15.4-web/css/all.css'
+import { Link } from "react-router-dom";
 
 function Diagnosticar() {
 
     return ( 
 
-        <div id="cabeceraSintomasBoton-Reglas">
+    <div id="fondo-Diagnosticar">
 
-        <div id="cajaLabelSintoma-Reglas">
-            <label>Diagnosticar</label>
+        <div id="cabeceraDiagnosticar-Diagnosticar">
+            
+            <div id="cajaLabelDiagnosticar-Diagnosticar">
+                <label>Diagnosticar</label>
+            </div>
+
         </div>
-        
+
+        <div id="cajaDiagnostico-Diagnosticar">
+
+            <div id="cajalabelSintoma-Diagnosticar">
+                <label id="labelSintoma-Diagnosticar">Sintoma</label>
+            </div>
+
+            <div id="cajaRegla-Diagnosticar">
+                <label>1. ¿La mitad de un lunar de nacimiento no es igual a la otra mitad?</label>
+            </div>
+
+        </div>
+
     </div>
+    
     );
 }
 
