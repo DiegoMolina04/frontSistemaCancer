b8ae468b548aadc051e82079aeb34773131c66e5
Diego Molina <diegomolina_30@hotmail.com>
2022-09-25 11:12:58 -0500

    Limpieza+Comentarios Login/useLogin

--- src/capaNegocio/logicaNegocio/useLogin.js
+++ src/capaNegocio/logicaNegocio/useLogin.js
@@ -1,10 +1,11 @@
-import React, { Fragment, useState, useContext, Component, useEffect } from 'react';
-//import datosBody from '../../capaDatos/Post/datosBody.js';
+//React
+import React, { useState, useContext, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
+//Context
 import { UserContext } from "../context/UserContext.js";
-import { Redirect, Switch, NavLink, useHistory } from "react-router-dom";
-//import { useNavigate } from "@reach/router"
+//Datos
 import postBody from '../../capaDatos/Post/postBody.js';
-
+//Componentes
 import ComponenteCorreo from '../../capaPresentacion/vista/Login/ComponenteCorreo.js';
 import ComponenteCorreoFijo from '../../capaPresentacion/vista/Login/ComponenteCorreoFijo.js';
 import ComponenteContraseña from '../../capaPresentacion/vista/Login/ComponenteContraseña.js';
@@ -13,251 +14,150 @@ import MostrarMensaje from '../../capaPresentacion/vista/ComponentesComunes/Most
 
 const useLogin = () => {
   
+  //Contextos
   const { token, setToken } = useContext(UserContext);
   const { es_admin, setEs_admin } = useContext(UserContext);
   const { correo, setCorreo } = useContext(UserContext);
   const { cambiarEstado, setCambiarEstado } = useContext(UserContext);
   const { datosGuardados, setDatosGuardados } = useContext(UserContext);
-
-  const [inputCorreo, setInputCorreo] = useState(<ComponenteCorreo />);
-  const [mostrarMensaje, setMostrarMensaje] = useState("");
-  const [inputContraseña, setInputContraseña] = useState("");
-  const [btnRegresar, setBtnRegresar] = useState("");
+  //Componentes
+  const [componenteCorreo, setComponenteCorreo] = useState(<ComponenteCorreo />);
+  const [componenteMostrarMensaje, setComponenteMostrarMensaje] = useState("");
+  const [componenteContraseña, setComponenteContraseña] = useState("");
+  const [componenteBtnRegresar, setComponenteBtnRegresar] = useState("");
+  //Guarda datos
   const [correoLocal, setCorreoLocal] = useState("");
 
-
-  let login = true;
-
-  //const navigate = useNavigate();
-  const history = useHistory();
-  let respuestaServidor = "";
+  const history = useHistory(); //Se usa para poder redireccionar
+  let respuestaServidor = ""; //Guarda respuesta del servidor
 
   const verificarUsuario = async (e, datos) => {
 
     e.preventDefault();
-    /*console.log("Datos desde el verificar")
-    console.log(datos.email)*/
-    console.log("Datos guardados");
-    console.log(datos.contraseña);
     setCambiarEstado("");
-
     const url = "https://secure-brushlands-86892.herokuapp.com/v1/users/login";
+
     try {
       if (correoLocal == "") { //Se verifica si el estado correoLocal esta vacia para saber si se debe guardar correo
-        console.log("Es nulo");
-
         respuestaServidor = await postBody(datos, url);
+
         if (respuestaServidor.code == 400) { //Se verifica si es un correo registrado, de serlo, se guarda
-          /*console.log("Reeeeeeespuesta");
-          console.log(respuestaServidor)
-          console.log(datos.email)*/
-          setCorreoLocal(datos.email)
+          setCorreoLocal(datos.email);
+
         } else {
 
         }
       } else { //Significa que el correo ya esta guardado y se debe armar el array con el correo
-        //console.log("NO Es nulo");
         const contraseña = datos.contraseña
         const array = { "email": correoLocal, "contraseña": contraseña }
-        console.log("Datos a enviar con contraseña");
-        console.log(array);
         respuestaServidor = await postBody(array, url);
+
       }
-
-      /*console.log("Datos a enviar");
-      const respuestaServidor = await datosLogin(datos);
-      console.log("Respuesta del servidor");
-      console.log(respuestaServidor)*/
-
       const datoCorreo = datos.email
-
-      /*if(login){
-        setInputCorreo(<ComponenteCorreoFijo email={datosModificado}/>);
-        setMostrarMensaje("Es una prueba");
-        setInputContraseña(<ComponenteContraseña/>);
-        setBtnRegresar(<ComponenteBotonRegresar/>);
-      }*/
-
-
-      //const datosModificado = datos.email
-      /*console.log("Datos modificados");
-      console.log(datosModificado)*/
-      //setComponenteCorreo(<ComponenteCorreoFijo email={datosModificado}/>)
-
-      /************************************************************************ */
-
-
 
       /*Se verifica si tiene token, si lo tiene, se redirecciona a la aplicación*/
       if (respuestaServidor.message == undefined && respuestaServidor.result.token !== undefined) {
-        /*console.log("Entre al 200 !");
-        console.log("Es indefinido, pero este es el token: "+respuestaServidor.result.token);*/
         setToken(respuestaServidor.result.token);
         setEs_admin(respuestaServidor.result.es_admin);
-        /*console.log("Este es el token guardado "+token);
-        console.log("Este es el es_admin guardado "+es_admin);*/
-        //setRespuestaServidor("");
-        //return <Redirect to="/plataforma/diagnosticar" />
         history.push("/plataforma/diagnosticar");
 
       } else if (respuestaServidor.status !== undefined && respuestaServidor.status === 200) {
-
-        //setMostrarMensaje("Contraseña creada correctamente.");
-        setMostrarMensaje(<MostrarMensaje mensaje={"Contraseña creada correctamente."} />);
+        setComponenteMostrarMensaje(<MostrarMensaje mensaje={"Contraseña creada correctamente."}/>);
 
       } else {
-        /*console.log("Entro en el Else y este es el mensaje "+respuestaServidor.message);*/
-
         switch (respuestaServidor.code) {
 
           /*Muestra campo contraseña cuando contraseña vacia y verificación de email -> Please verify your credentials*/
-          case 400:/*400*/
-
+          case 400:
             if (datos.email == "") {
+              setComponenteMostrarMensaje(<MostrarMensaje mensaje={"Debe ingresar un correo."} />);
 
-              //setMostrarMensaje("Debe ingresar un correo.");
-              setMostrarMensaje(<MostrarMensaje mensaje={"Debe ingresar un correo."} />);
             } else {
-
               setCorreo(datos.email);
-
-              //setMostrarMensaje("Ingrese contraseña valida.");
-
-              setMostrarMensaje(<MostrarMensaje mensaje={"Ingrese contraseña valida."} />);
+              setComponenteMostrarMensaje(<MostrarMensaje mensaje={"Ingrese contraseña valida."} />);
 
               /*Se carga componenteCorreo con el correo setteado y sin posibilidad de editar*/
-              /*componenteCorreo = <div className="input-group">
-                <div className="input-group-text" id="btnGroupAddon"><i className="fas fa-user"></i></div>
-                <input id="usuario" type="text" className="form-control" placeholder="Email" onChange={handleInputChange} name="email" title="Ingrese su correo" readonly="readonly" value={datos.email} />
-              </div>*/
-              setInputCorreo(<ComponenteCorreoFijo email={datoCorreo} />);
+              setComponenteCorreo(<ComponenteCorreoFijo email={datoCorreo} />);
 
               /*Se carga componenteContraseña para digitar contraseña*/
-              /*componenteContraseña = <div className="input-group">
-                <div className="input-group-text" id="btnGroupAddon"><i className="fas fa-key"></i></div>
-                <input id="contraseña" type="password" className="form-control" placeholder="Contraseña" onChange={handleInputChange} name="contraseña" title="Ingrese su contraseña" required />
-              </div>*/
-              setInputContraseña(<ComponenteContraseña />);
-
-              //componenteBtnRegresar = <button id="btnRegresar" type="button" class="btn btn-secondary" onClick={estadoInicial} title="Regresar">Regresar</button>
-              setBtnRegresar(<ComponenteBotonRegresar />);
+              setComponenteContraseña(<ComponenteContraseña />);
+              setComponenteBtnRegresar(<ComponenteBotonRegresar />);
 
 
             }
 
-            /*setRespuestaServidor("");*/
-
             break;
 
           /*Crear contraseña primera vez -> Storage error*/
-          case 500: /*500*/
+          case 500:
 
             if (respuestaServidor.message == "data and hash arguments required") {
               setCorreo(datos.email);
+              setComponenteMostrarMensaje(<MostrarMensaje mensaje={"Ingrese contraseña valida."} />);
 
-              //setMostrarMensaje("Ingrese contraseña valida.");
-
-              setMostrarMensaje(<MostrarMensaje mensaje={"Ingrese contraseña valida."} />);
-
-              /*Se carga componenteCorreo con el correo setteado y sin posibilidad de editar*/
-              /*componenteCorreo = <div className="input-group">
-                <div className="input-group-text" id="btnGroupAddon"><i className="fas fa-user"></i></div>
-                <input id="usuario" type="text" className="form-control" placeholder="Email" onChange={handleInputChange} name="email" title="Ingrese su correo" readonly="readonly" value={datos.email} />
-              </div>*/
-              setInputCorreo(<ComponenteCorreoFijo email={datoCorreo} />);
+              //Se carga componenteCorreo con el correo setteado y sin posibilidad de editar
+              setComponenteCorreo(<ComponenteCorreoFijo email={datoCorreo} />);
 
               /*Se carga componenteContraseña para digitar contraseña*/
-              /*componenteContraseña = <div className="input-group">
-                <div className="input-group-text" id="btnGroupAddon"><i className="fas fa-key"></i></div>
-                <input id="contraseña" type="password" className="form-control" placeholder="Contraseña" onChange={handleInputChange} name="contraseña" title="Ingrese su contraseña" required />
-              </div>*/
-              setInputContraseña(<ComponenteContraseña />);
-
-              //componenteBtnRegresar = <button id="btnRegresar" type="button" class="btn btn-secondary" onClick={estadoInicial} title="Regresar">Regresar</button>
-              setBtnRegresar(<ComponenteBotonRegresar />);
+              setComponenteContraseña(<ComponenteContraseña />);
+              setComponenteBtnRegresar(<ComponenteBotonRegresar />);
+              
             } else {
               setCorreo(datos.email);
-              //setRespuestaServidor("");
               setDatosGuardados("");
-              //return <Redirect to="/crearcontraseña" />
               history.push("/crearcontraseña");
+
             }
 
             break;
 
           /*Email no existe || Campos vacios -> User with provided email dont exist*/
-          case 404:/*404*/
-
+          case 404:
             if (datos.email === "") { /*Campo email vacio*/
+              setComponenteMostrarMensaje(<MostrarMensaje mensaje={"Debe ingresar un correo."} />);
 
-              /*console.log("Correo vacio");*/
-              //componenteMostrarMensaje = <h6>Debe ingresar un correo</h6>;
-
-              //setMostrarMensaje("Debe ingresar un correo.");
-
-              setMostrarMensaje(<MostrarMensaje mensaje={"Debe ingresar un correo."} />);
-
-              /*setRespuestaServidor("");*/
-              /*setRespuestaServidor("");*/
-              /*componenteMostrarMensaje = "";*/
             } else { /*Email no encontrado*/
+              setComponenteMostrarMensaje(<MostrarMensaje mensaje={"Ingrese un correo registrado."} />);
 
-              /*console.log("Email no encontrado");*/
-              //componenteMostrarMensaje = <h6>Ingrese un correo registrado</h6>;
-
-              //setMostrarMensaje("Ingrese un correo registrado.");
-
-              setMostrarMensaje(<MostrarMensaje mensaje={"Ingrese un correo registrado."} />);
-
-              /*componenteMostrarMensaje = <h6>Ingrese un correo valido</h6>*/
-              /*setRespuestaServidor("");*/
             }
-            /*setRespuestaServidor("");*/
+            
             break;
 
         }
       }
 
     } catch (error) {
+      setComponenteMostrarMensaje(<MostrarMensaje mensaje={"Un error a sucedido."} />);
 
-      //setMostrarMensaje("Un error a sucedido.");
-      setMostrarMensaje(<MostrarMensaje mensaje={"Un error a sucedido."} />);
     }
-
 
   };
 
-
+  //Regresa al estado inicial
   const estadoInicial = async (e) => {
-
     e.preventDefault();
-    console.log("Funcione");
-
     setCambiarEstado("Estado inicial");
-
 
   }
 
+  //Cada vez que se ejecute cambiarEstado, se ejecuta el useEffect
   useEffect(() => {
-    console.log("Entre ak efecto");
-    console.log("El estado que llega al Login");
-    console.log(cambiarEstado);
     if (cambiarEstado == "Estado inicial") {
-      //setMostrarMensaje("");
       setCorreo("");
       setCorreoLocal("");
       setCambiarEstado("");
-      setMostrarMensaje(<MostrarMensaje mensaje={""} />);
-      setInputCorreo(<ComponenteCorreo />);
-      setInputContraseña("");
-      setBtnRegresar("");
-    } else if (cambiarEstado == "Contraseña creada correctamente") {
-      console.log("Entre desde el Login");
-      setMostrarMensaje(<MostrarMensaje mensaje={"Contraseña creada correctamente"} />);
-    }
-  }, [cambiarEstado])//}, [cambiarEstado])
+      setComponenteMostrarMensaje(<MostrarMensaje mensaje={""} />);
+      setComponenteCorreo(<ComponenteCorreo />);
+      setComponenteContraseña("");
+      setComponenteBtnRegresar("");
 
-  return { verificarUsuario, inputCorreo, mostrarMensaje, inputContraseña, btnRegresar, estadoInicial };
+    } else if (cambiarEstado == "Contraseña creada correctamente") {
+      setComponenteMostrarMensaje(<MostrarMensaje mensaje={"Contraseña creada correctamente"} />);
+
+    }
+  }, [cambiarEstado])
+
+  return { verificarUsuario, componenteCorreo, componenteMostrarMensaje, componenteContraseña, componenteBtnRegresar, estadoInicial };
 };
 
 export default useLogin;
--- src/capaPresentacion/vista/Login/Login.js
+++ src/capaPresentacion/vista/Login/Login.js
@@ -1,66 +1,65 @@
 import '../../css/Login.css'; //Estilos
 import '../../css/fontawesome-free-5.15.4-web/css/all.css' //Iconos
 import logo from '../../image/logo.png' //Logo universidad.
-import React, {useContext} from 'react'; //Importación de hooks.
+import React, { useContext } from 'react'; //Importación de hooks.
 import { UserContext } from "../../../../src/capaNegocio/context/UserContext.js"; //Para poder crear contextos.
 import useLogin from '../../../capaNegocio/logicaNegocio/useLogin'; //Logica negocio.
 
 function Login() {
-  
-//Contextos para globalizar datos
-//Datos guardados desde el handleChange y enviados a logica de negocio.
-const {datosGuardados, setDatosGuardados} = useContext(UserContext); 
 
-//Custom hook para comunicación con lógica de negocio.
-const {verificarUsuario, inputCorreo, mostrarMensaje, inputContraseña, btnRegresar} = useLogin();
+  //Contextos para globalizar datos
+  //Datos guardados desde el handleChange y enviados a logica de negocio.
+  const { datosGuardados, setDatosGuardados } = useContext(UserContext);
 
-/*Componente para mostrar el input de ingresar contraseña
-una vez se confirma que el email esta registrado*/
-let componenteContraseña = <div>{inputContraseña}</div>
+  //Custom hook para comunicación con lógica de negocio.
+  const { verificarUsuario, componenteCorreo, componenteMostrarMensaje, componenteContraseña, componenteBtnRegresar } = useLogin();
 
-//Componente para mostrar el input del correo desbloqueado o bloqueado.
-let componenteCorreo = <>{inputCorreo}</>
+  //Componente para mostrar el input de ingresar contraseña una vez se confirma que el email esta registrado
+  //componenteContraseña
 
-/*Componente para mostrar mostrar botón regresar una vez que se
-confirma que el email esta registrado*/
-let componenteBtnRegresar= <>{btnRegresar}</>
+  //Componente para mostrar el input del correo desbloqueado o bloqueado.
+  //componenteCorreo
 
-//Componente para mostrar el mensaje informativo en el lugar que se desea.
-let componenteMostrarMensaje = <h6>{mostrarMensaje}</h6> 
+  //Componente para mostrar mostrar botón regresar una vez que se confirma que el email esta registrado
+  //componenteBtnRegresar
+
+  //Componente para mostrar el mensaje informativo en el lugar que se desea.
+  //componenteMostrarMensaje
 
   return (
-  
+
     <div id="seccion">
 
       <div id="caja">
-      
+
         <div id="cajaLogo">
 
-            <img id="logo" src={logo} alt=""/>
+          <img id="logo" src={logo} alt="" /> {/*Muestra el logo de la universidad*/}
 
-        </div>{/*(e) => {LogicaLogin(e)}*/}{/*(e) => verificarUsuario(e, datos)*/}
+        </div>
 
-        <hr id="linea"/>{/*(e) => {setRespuestaServidor(LogicaLogin.consultarUsuario(e, datos))}*/}{/*(e) => {Prueba(e)}*/}
+        <hr id="linea" />
 
         <form className="row" onSubmit={(e) => verificarUsuario(e, datosGuardados)}>{/*Se envian los datos a logica de negocio*/}
 
           <p>Inicio de sesión</p>
 
-          {componenteMostrarMensaje}
-          {componenteCorreo}
+          <h6>{componenteMostrarMensaje}</h6> {/*Muestra mensaje informativo*/}
 
-          {componenteContraseña}       
-          
+          {componenteCorreo} {/*Muestra el correo para editar y el fijo*/}
+
+          <div>{componenteContraseña}</div> {/*Muestra el input para ingresar la contraseña*/}
+
           <div id="cajaBoton">
-              
-              {componenteBtnRegresar}
-              
-              <button id="btnEnviar" type="submit" class="btn btn-success" title="Iniciar sesión">Enviar</button>
-              
-    
+
+            {componenteBtnRegresar} {/*Muestra el botón regresar una vez se fija el correo*/}
+
+            <button id="btnEnviar" type="submit" class="btn btn-success" title="Iniciar sesión">Enviar</button>
+
           </div>
-        
+
         </form>
+
       </div>
 
     </div>
