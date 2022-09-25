2afa9cc0da77eb668098f78ee2ad83a89be7e29d
Diego Molina <diegomolina_30@hotmail.com>
2022-09-25 11:41:16 -0500

    Limpieza+Comentario useChange CrearContraseña

--- src/capaPresentacion/vista/CrearContraseña/useChange.js
+++ src/capaPresentacion/vista/CrearContraseña/useChange.js
@@ -1,43 +1,32 @@
-import {useState, useContext, useEffect} from 'react';
+//React
+import { useState, useContext, useEffect } from 'react';
+//Context
 import { UserContext } from '../../../capaNegocio/context/UserContext';
 
 const useChange = () => {
 
-    const {respuestaServidor, setRespuestaServidor} = useContext(UserContext);
-    const [datos, setDatos] = useState({contraseña:'', Recontraseña:''})
-    const {datosGuardados, setDatosGuardados} = useContext(UserContext);
+    //Estado para guardar datos
+    const [datos, setDatos] = useState({ contraseña: '', Recontraseña: '' })
+    //Contexto
+    const { datosGuardados, setDatosGuardados } = useContext(UserContext);
 
-    const handleInputChange = (event) => {
-        
+    const handleInputChange = (event) => { //Captura datos ingresados
+
         event.preventDefault();
-        console.log(event.currentTarget.name)
-        console.log(event.currentTarget.value)
-        console.log("Hola desde useChange");
-  
-        console.log("Datos antes")
-        //console.log(respuestaServidor)
-        console.log(datos)
         setDatos({
             ...datos,
             [event.currentTarget.name]: event.currentTarget.value
         })
 
-        console.log("Datos despues")
-        console.log(datos)
-
-        //obtenerDatos(datos);
-        //console.log(respuestaServidor)
-
-
     }
-    
+
     //Cada vez que se escriben los datos, se guarda en el contexto para ser enviado a la logica de negocio.
     useEffect(() => {
         setDatosGuardados(datos);
     }, [datos])
-      
-      return { handleInputChange };
-     
+
+    return { handleInputChange };
+
 };
 
 export default useChange;
